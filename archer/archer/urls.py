117c7b47b2140d349895de9db8ea5f1c50b0f881
Yehor Huskov <y.huskov@pinta.com.ua>
2018-10-24 13:22:44 +0300

    Added a 'login/' view for getting authentication tokens in exchange for username and password.

--- archer/archer/urls.py
+++ archer/archer/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('', include('getpackage.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
-    path('docs/', include_docs_urls(title='My API title'))
+    path('docs/', include_docs_urls(title='My API title')),
+    path('login/', views.obtain_auth_token),
 ]
