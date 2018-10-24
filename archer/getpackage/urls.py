c9fd48aed35b1acef874e5e35522df547b3d65ae
Yehor Huskov <y.huskov@pinta.com.ua>
2018-10-24 07:48:08 +0300

    Simplified the definition of REST views.

---
+++ archer/getpackage/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from getpackage import views
+
+urlpatterns = [
+    path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>/', views.PostDetail.as_view()),
+]
+
